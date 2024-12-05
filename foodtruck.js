4bf7e96960fed5540a0f8daf541f76572945b169
Hanna Seldén <hanna_lotta@hotmail.com>
2024-12-05 21:58:44 +0100

    fetch key

--- foodtruck.js
+++ foodtruck.js
@@ -1,6 +1,6 @@
-https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/
+//https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/
 //API key
-POST/keys
+/*POST/keys
 //tenants
 POST/tenants
 //menu
@@ -13,3 +13,38 @@ GET/{tenant}/orders
 GET/orders/{id}
 //receipt
 GET/receipts/{id}
+*/
+
+	/*const menuButton = document.querySelector('#menu-button')
+	const url = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys'
+	menuButton.addEventListener('click', async () => {
+	
+})*/
+	const url = 'https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys'
+	const bodyToSend = {
+    name: 'Hanna'
+}
+	const options = {
+    method: 'POST',
+	headers: {
+        "Content-Type": 'application/json',  // vi skickar JSON i body
+        
+    },
+
+    body: JSON.stringify(bodyToSend)
+}
+	
+	try {
+	const response = await fetch(url,options)
+		console.log('status är: ', response.status);
+		
+		const data = await response.json()
+		console.log('data från API: ', data);
+		
+
+} catch (error) {
+	console.log('det gick inte ...', error.message);
+	
+}
+
+const key = key : "yum-edVCa1E6zDZRztaq"
