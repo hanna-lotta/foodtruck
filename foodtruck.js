1e239095b73055e9ce58122cc7622ae1d8af2de1
Hanna Seldén <hanna_lotta@hotmail.com>
2024-12-05 20:09:12 +0100

    designskiss början på implementering

---
+++ foodtruck.js
@@ -0,0 +1,15 @@
+https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/
+//API key
+POST/keys
+//tenants
+POST/tenants
+//menu
+GET/menu
+GET/menu/{id}
+
+//order
+POST/{tenant}/orders
+GET/{tenant}/orders
+GET/orders/{id}
+//receipt
+GET/receipts/{id}
